2148156a774ed497c3aee9c85cd4ea879d16f2fa
Sergey Z <zassipkin.com>
2022-08-08 21:17:16 +0300

    Initial commit for the game

---
+++ Seminar2/Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+ static void SelectRase()
+    {
+        string[] namesRase = {"People", "Elfs", "Orks", "Goblins", "Dwarf"};
+        for (int i = 0; i <namesRase.Length-1; i++)
+    {
+        Console.Write(i+1 + " - " + namesRase[i] + ", ");
+    }
+        Console.WriteLine(namesRase.Length + " - " + namesRase[namesRase.Length-1] + ".");
+        Console.WriteLine("What is your race?");
+        int numberRace = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your race is " + namesRase[numberRace-1]);
+    }
+
+    static void SelectSex()
+    {
+        string[] namesSex = {"Man", "Woman"};
+        for (int i = 0; i <namesSex.Length-1; i++)
+    {
+        Console.Write(i+1 + " - " + namesSex[i] + ", ");
+    }
+        Console.WriteLine(namesSex.Length + " - " + namesSex[namesSex.Length-1] + ".");
+        Console.WriteLine("What is your sex?");
+        int numberSex = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("You are a " + namesSex[numberSex-1]);
+    }
+    static void Main(string[] args)
+    {
+        SelectRase();
+        SelectSex();
+    }
+}
