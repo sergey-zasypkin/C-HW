08610b0dbb2e3db1e416e9dc143fa8ec1bb60e1d
Sergey Z <zassipkin.com>
2022-08-22 20:57:32 +0300

    Test tasks

---
+++ Seminar5/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте одномерный массив из 123 случайных чисел
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] numbers = new int[123];
+
+static int[] createArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 1000);
+    }
+    return numbers;
+}
+
+createArray(numbers);
+
+int Count = 0;
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] > 10 && numbers[i] < 99)
+Count++;
+
+Console.WriteLine("Count of numbers between 10 & 99 is " + Count);
+
---
+++ Seminar5/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+
+int[] numbers = new int[6];
+
+static int[] createArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 10);
+    }
+    return numbers;
+}
+
+static void writeArray(int[] numbers)
+{
+    foreach(int number in numbers)
+    {
+        Console.Write(number + " ");
+    }
+}
+
+writeArray(createArray(numbers));
+Console.WriteLine();
+
+int product = 0;
+int min = 0;
+int max = numbers.Length-1;
+
+while(min < max)
+{
+    product = numbers[min] * numbers[max];
+    min++;
+    max--;
+    Console.Write(product + " ");
+}
+
+// решение работает только для четного числа элементов массива(((
