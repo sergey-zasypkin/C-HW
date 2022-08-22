45f621bea8575b1fd39c56297708262d7d988c14
Sergey Z <zassipkin.com>
2022-08-22 21:09:47 +0300

    Решение препода добавлено

--- Seminar5/Task1/Program.cs
+++ Seminar5/Task1/Program.cs
@@ -21,3 +21,27 @@ Count++;
 
 Console.WriteLine("Count of numbers between 10 & 99 is " + Count);
 
+
+// int[] startArray = GetArray(123, 0, 1000);
+// WriteLine($"Êîëè÷åñòâî ýëåìåíòîâ â îòðåçêå [10;99] = {GetCountElements(startArray, 10, 99)}");
+
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return res;
+// }
+
+// int GetCountElements(int[] array, int leftRange, int rigthRange)
+// {
+//     int count = 0;
+//     foreach (var item in array)
+//     {
+//         if (item >= leftRange && item <= rigthRange) count++;
+//     }
+//     return count;
+// }
--- Seminar5/Task2/Program.cs
+++ Seminar5/Task2/Program.cs
@@ -37,3 +37,36 @@ while(min < max)
 }
 
 // решение работает только для четного числа элементов массива(((
+
+
+//     Write("Ââåäèòå ýëåìåíòû ìàññèâà ÷åðåç ïðîáåë: ");
+// string elements = ReadLine();
+// int[] baseArray = GetArrayFromString(elements);
+
+// WriteLine(String.Join(" ", ResultArray(baseArray)));
+
+// int[] GetArrayFromString(string stringArray)
+// {
+//     string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+//     int[] res = new int[nums.Length];
+
+//     for (int i = 0; i < nums.Length; i++)
+//     {
+//         res[i] = int.Parse(nums[i]);
+//     }
+//     return res;
+// }
+
+// int[] ResultArray(int[] array)
+// {
+//     int size = array.Length / 2;
+//     if (array.Length % 2 == 1) size++;
+//     int[] result = new int[size];
+//     for (int i = 0; i < array.Length / 2; i++)
+//     {
+        
+//         result[i] = array[i] * array[array.Length - 1 - i];
+//     }
+//     if (array.Length % 2 == 1) result[size-1] = array[array.Length / 2];
+//     return result;
+// }
